0a3533c2fdff2165e47725732d95d9287bd461d5
Bosepandi <bosepandip892005_bai27@mepcoeng.ac.in>
2026-06-16 15:42:50 +0530

    Time: 7 ms (96.70%) | Memory: 19.6 MB (69.04%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solveNQueens(self, n: int) -> list[list[str]]:
+        res = []
+        board = [["."] * n for _ in range(n)]
+        cols = set()
+        pos_diag = set()
+        neg_diag = set() 
+        def place_queen(r):
+            if r == n:
+                res.append(["".join(row) for row in board])
+                return
+            for c in range(n): 
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue 
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                board[r][c] = "Q" 
+                place_queen(r + 1)
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+                board[r][c] = "." 
+        place_queen(0) 
+        return res
